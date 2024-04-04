52add61b7758fc3c3a0ae4d7a1ae194e7ee2e6d8
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-05 01:06:20 +0630

    reafactor: add error handling in controller file

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -24,7 +24,17 @@ async function getSingleWorkout(req, res) {
 
 // create a new workout
 async function createWorkout(req, res) {
-  // const { title, load, reps } = req.body;
+  const { title, load, reps } = req.body;
+
+  const emptyFields = [];
+  if (!title) emptyFields.push("title");
+  if (!load) emptyFields.push("load");
+  if (!reps) emptyFields.push("reps");
+  if (emptyFields.length > 0)
+    return res
+      .status(400)
+      .json({ error: "Please fill all fields", emptyFields });
+
   try {
     const newWorkout = await Workout.create(req.body);
     res.status(200).json(newWorkout);
