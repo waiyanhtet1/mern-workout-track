44042a97faf0ba321b41430f2a4a2c01f90be696
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-04 15:36:48 +0630

    add: get all data and style workout list

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import WorkoutDetail from "../components/WorkoutDetail";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    const fetchWorkout = async () => {
+      const response = await fetch("/api/workouts");
+      const result = await response.json();
+
+      if (response.ok) setWorkouts(result);
+    };
+    fetchWorkout();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <WorkoutDetail workout={workout} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
