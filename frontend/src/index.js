3646204c4893e9ae77cfcf1c3d17d18274ec2795
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-05 15:21:46 +0630

    setup: authContext in react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { WorkoutContextProvider } from "./context/workoutContext";
+import { AuthContextProvider } from "./context/authContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <WorkoutContextProvider>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </WorkoutContextProvider>
     </BrowserRouter>
   </React.StrictMode>
