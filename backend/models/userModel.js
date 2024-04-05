52b4c11054ccff205c66dbf3bd24c6fc087e7c3c
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-05 15:09:11 +0630

    setup: jwt for authentication process

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -13,7 +14,14 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// signup static method
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) throw Error("All field must be fill");
+  if (!validator.isEmail(email)) throw Error("Email is not valid");
+  if (!validator.isStrongPassword(password))
+    throw Error("Password not strong enough");
+
   const isExit = await this.findOne({ email });
   if (isExit) throw Error("Email is already exits");
 
@@ -21,6 +29,19 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+  return user;
+};
+
+// login static method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) throw Error("All field must be fill");
+
+  const user = await this.findOne({ email });
+  if (!user) throw Error("Incorrect Email");
+
+  const compare = await bcrypt.compare(password, user.password);
+  if (!compare) throw Error("Incorrect Password");
+
   return user;
 };
 
