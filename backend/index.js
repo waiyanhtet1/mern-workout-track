0fdeaec1b2ffe5e345942ab461f7e09067bee456
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-07 23:19:56 +0630

    refactor: added some minor changes

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@ const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // express app
 const app = express();
 
 // middleware
 app.use(express.json());
+
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
